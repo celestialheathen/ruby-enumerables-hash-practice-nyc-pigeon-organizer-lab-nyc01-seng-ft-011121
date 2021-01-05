6b55bb1d365356f1cd875fa63d2f023c594a2889
Jingchao Yu <jingchaoyu@hotmail.com>
2021-01-05 16:43:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,41 +3,26 @@ def nyc_pigeon_organizer(data)
   pigeon_list = Hash.new
 
   data[:gender][:male].each { |name|
-    pigeon_list[name] = {}
-    pigeon_list[name][:gender] = ["male"]
-    pigeon_list[name][:color] = []
+    pigeon_list[name] = {:color => [], :gender => ["male"], :lives => []}
   }
   data[:gender][:female].each { |name|
-    pigeon_list[name] = {}
-    pigeon_list[name][:gender] = ["female"]
-    pigeon_list[name][:color] = []
+    pigeon_list[name] = {:color => [], :gender => ["female"], :lives => []}
   }
   
  # Insert the color/lives dictionaries into each pigeon key.
-  data[:color][:purple].each { |name|
-    pigeon_list[name][:color].push("purple")
-  }
-  data[:color][:grey].each { |name|
-    pigeon_list[name][:color].push("grey")
-  }
-  data[:color][:white].each { |name|
-    pigeon_list[name][:color].push("white")
-  }
-  data[:color][:brown].each { |name|
-    pigeon_list[name][:color].push("brown")
+  data[:color].collect { |color, names|
+    names.each {  |name|
+      pigeon_list[name][:color] << "#{color}"
+    }
   }
 
 
   #check each pigeon data/lives and insert into pigeon[lives] by reversing the info
-  data[:lives]["Subway"].each { |name|
-    pigeon_list[name][:lives] = "Subway"  }
-  data[:lives]["Central Park"].each { |name|
-    pigeon_list[name][:lives] = "Central Park" }
-  data[:lives]["Library"].each { |name|
-    pigeon_list[name][:lives] = "Library" }
-  data[:lives]["City Hall"].each { |name|
-    pigeon_list[name][:lives] = "City Hall" }
-
+  data[:lives].collect { |location, names|
+  names.each {  |name|
+    pigeon_list[name][:lives] << "#{location}"
+  }
+}
 
   return pigeon_list
 end
